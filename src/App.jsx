d250e7c16c91682b442dbe504d87971a24256f4b
Saqlain Mansab <120156817+saqlainrai@users.noreply.github.com>
2024-06-13 05:09:30 +0000

    Updated Details

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Saqlain Mansab",
+  title: "Full Stack Web Developer",
+  email: "saqlainmansab786@gmail.com",
+  gitHub: "saqlainrai",
+  instagram: "saqlainrai",
+  linkedIn: "saqlainmansab",
   medium: "",
   twitter: "microsoft",
-  youTube: "Code",
+  youTube: "saqlainmansab786",
 };
 
 const primaryColor = "#4E567E";
