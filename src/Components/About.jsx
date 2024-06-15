843a2844d6c98586d57969bce3dc7c1299bac2a5
Saqlain Mansab <120156817+saqlainrai@users.noreply.github.com>
2024-06-15 04:23:12 +0000

    Updated the details

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,20 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Computer Science student studying at University of Engineering and Technology, Lahore. I love to learn about the new technologies and skills.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "C++/Python Programming",
+  "C# Windows Form",
+  "Full Stack Web Development",
+  "Microsoft Office 365", 
+  "Adobe Creative Cloud",
+  "Microsoft Power BI",
+  "Graphic designing"
 ];
 
 /**
@@ -45,7 +46,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems in new creative ways to drive innovation. I believe in lifelong learning and seek opportunities to expand my knowledge";
 
 const About = () => {
   return (
